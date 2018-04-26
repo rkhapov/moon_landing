a47b86e066a1facda013f2b998124bece08e697f
Марина Коновалова <konovalova.owl@gmail.com>
2018-04-27 00:58:48 +0500

    Added operator "*", methods Lenght, Angle for class Vector

--- MoonLanding/Tools/Vector.cs
+++ MoonLanding/Tools/Vector.cs
@@ -1,4 +1,6 @@
-﻿namespace MoonLanding.Tools
+﻿using System;
+
+namespace MoonLanding.Tools
 {
     public class Vector
     {
@@ -15,14 +17,34 @@
             return new Vector { X = x, Y = y };
         }
 
-        public static Vector operator +(Vector v1, Vector v2)
+        public static Vector operator + (Vector v1, Vector v2)
         {
             return Vector.Create(v1.X + v2.X, v1.Y + v2.Y);
         }
 
-        public static Vector operator -(Vector v1, Vector v2)
+        public static Vector operator - (Vector v1, Vector v2)
         {
             return Create(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector operator * (Vector v, double a)
+        {
+            return Create(v.X * a, v.Y * a);
+        }
+
+        public static Vector operator *(double a, Vector v)
+        {
+            return v * a;
+        }
+
+        public static double Length(Vector v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y);
+        }
+
+        public static double Angle(Vector v)
+        {
+            return Math.Atan2(v.X, v.Y);
         }
     }
 }
