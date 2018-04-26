9f1c3a9f87fd280a3f49b9358611d8cdaf8c4973
koksharov1998 <koksharov1998@ya.ru>
2018-04-26 23:21:08 +0500

    Added operator "+" for class Vector.

--- MoonLanding/Tools/Vector.cs
+++ MoonLanding/Tools/Vector.cs
@@ -12,7 +12,12 @@
 
         public static Vector Create(double x, double y)
         {
-            return new Vector {X = x, Y = y};
+            return new Vector { X = x, Y = y };
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
         }
     }
 }
