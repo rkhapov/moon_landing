8a2272982cb42ef558d9012a3d07c43f2e10aca3
Марина Коновалова <konovalova.owl@gmail.com>
2018-04-26 23:59:03 +0500

    Added operator "-" for class Vector

--- MoonLanding/Tools/Vector.cs
+++ MoonLanding/Tools/Vector.cs
@@ -19,5 +19,10 @@
         {
             return new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
         }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return Create(v1.X - v2.X, v1.Y - v2.Y);
+        }
     }
 }
