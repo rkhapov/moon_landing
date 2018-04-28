483bbc0ca92a3888937fe4c17a3e282e623a3c89
koksharov1998 <koksharov1998@ya.ru>
2018-04-28 16:54:34 +0500

    Added class "Level"

---
+++ MoonLanding/Level.cs
@@ -0,0 +1,22 @@
+﻿using MoonLanding.Physics;
+using System.Collections.Generic;
+
+namespace MoonLanding
+{
+    public class Level
+    {
+        public Landscape Landscape { get; }
+        public List<IPhysObject> Objects { get; }
+
+        private Level(Landscape landscape, List<IPhysObject> objects)
+        {
+            Landscape = landscape;
+            Objects = objects;
+        }
+
+        public static Level Create(Landscape landscape, List<IPhysObject> objects)
+        {
+            return new Level(landscape, objects);
+        }
+    }
+}
