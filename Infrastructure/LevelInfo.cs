15941ec89077e198aac7cb99e93e614218d3cb92
koksharov1998 <koksharov1998@ya.ru>
2018-04-28 22:59:15 +0500

    Improved reading from file and added writing LevelInfo to file

--- Infrastructure/LevelInfo.cs
+++ Infrastructure/LevelInfo.cs
@@ -3,27 +3,47 @@ using System.IO;
 
 namespace Infrastructure
 {
-    class LevelInfo
+    public class LevelInfo
     {
         public Vector StartPosition { get; set; }
         public Vector StartVelocity { get; set; }
         public double StartFuel { get; set; }
+
+        public LevelInfo(Vector startPosition, Vector startVelocity, double startFuel)
+        {
+            StartPosition = startPosition;
+            StartVelocity = startVelocity;
+            StartFuel = startFuel;
+        }
 
         public LevelInfo(string path)
         {
             var lines = File.ReadAllLines(path);
             try
             {
-                var line = lines[0].Split();
-                StartPosition = Vector.Create(double.Parse(line[0]), double.Parse(line[1]));
-                line = lines[1].Split();
-                StartVelocity = Vector.Create(double.Parse(line[0]), double.Parse(line[1]));
+                StartPosition = ReadVector(lines[0].Split());
+                StartVelocity = ReadVector(lines[1].Split());
                 StartFuel = double.Parse(lines[2]);
             }
             catch
             {
                 throw new InvalidDataException("Invalid file format.");
             }
+        }
+
+        private Vector ReadVector(string[] line)
+        {
+            return Vector.Create(double.Parse(line[0]), double.Parse(line[1]));
+        }
+
+        private string[] ToStringArray()
+        {
+            return new string[] { StartPosition.ToString(), StartVelocity.ToString(), StartFuel.ToString() };
+        }
+
+        public void WriteLevelInFile(LevelInfo levelInfo, string fileName)
+        {
+            File.WriteAllLines(fileName, levelInfo.ToStringArray());
         }
     }
 }
