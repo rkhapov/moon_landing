1491c2c4df880fda1effac852a9125638587a344
Roman Khapov <r.khapov@yandex.ru>
2018-05-04 05:08:29 +0300

    wip

--- Core.Tests/PhysicsTests.cs
+++ Core.Tests/PhysicsTests.cs
@@ -22,20 +22,5 @@ namespace Core.Tests
 //            var ship = Ship.Create(100, new Size(2, 2), Vector.Zero, 5);
             // TODO
         }
-
-        [Test]
-        public void Physics_ShouldntUpdateLandscapesCoordinates()
-        {
-            var landscape = Landscape.CreateFromText(new List<string>
-            {
-                "......................",
-                "................******",
-                "...............*******",
-                "****.........*********",
-                "******....************",
-                "**********************"
-            }, chr => chr == '*' ? LandscapeCell.Ground : LandscapeCell.Empty);
-            // TODO
-        }
     }
 }
