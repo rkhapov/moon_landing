1fb80c39f6b51d4a953af3487dbd2f5c36bb03b3
koksharov1998 <koksharov1998@ya.ru>
2018-05-04 14:32:08 +0500

    Added LevelInfoTests

---
+++ Core.Tests/LevelInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core.Tests
+{
+    [TestFixture]
+    public class LevelInfoTests
+    {
+        [Test]
+        public void MakeFileAndCheckIt()
+        {
+
+        }
+    }
+}
